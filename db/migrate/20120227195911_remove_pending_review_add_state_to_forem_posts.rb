3de1324729ae3b46365f2b179bea545a2c0a343c
bagusyuu <bagusyuu@yahoo.com>
2012-05-30 15:05:00 +0700

    change default permission from pending_review into approved

--- db/migrate/20120227195911_remove_pending_review_add_state_to_forem_posts.rb
+++ db/migrate/20120227195911_remove_pending_review_add_state_to_forem_posts.rb
@@ -1,7 +1,7 @@
 class RemovePendingReviewAddStateToForemPosts < ActiveRecord::Migration
   def change
     remove_column :forem_posts, :pending_review
-    add_column :forem_posts, :state, :string, :default => 'pending_review'
+    add_column :forem_posts, :state, :string, :default => 'approved'
     add_index :forem_posts, :state
   end
 end
